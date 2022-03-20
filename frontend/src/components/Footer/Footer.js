f4bf9f2d8fb3b58f1c067de45ad0e8d2b609dc8b
Cam Tangalakis-Lippert <cami.lippert@gmail.com>
2022-03-19 22:08:19 -0700

    no font awesome

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <div className='devInfo'>
             <div className='devName'><strong>Cam Tangalakis</strong></div>
-            <a className='devIn' href='https://www.linkedin.com/in/cam-tangalakis-lippert-9b4a11129'><i class="fa fa-linkedin-square fa-lg" aria-hidden="false"></i></a>
-            <a className='devGit' href='https://github.com/CamTangalakis'><i class="fa fa-github-square fa-lg" aria-hidden="false"></i></a>
+            <a className='devIn' href='https://www.linkedin.com/in/cam-tangalakis-lippert-9b4a11129'>in</a>
+            <a className='devGit' href='https://github.com/CamTangalakis'>gh</a>
         </div>
     )
 }
